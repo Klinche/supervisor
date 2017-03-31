363ae686f505753402f2cb7b277ecd415e4c7a66
Daniel Brooks <dbrooks@klinche.com>
2017-03-31 15:34:22 -0700

    Adding in some special params

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,10 @@ set -e
 if [ "$1" = 'supervisord' ]; then
 	if [ "$SUPERVISOR_CONF" ]; then
         cp $SUPERVISOR_CONF /etc/supervisor/conf.d/supervisord.conf
+        sed -e "s|REDISPREFIX|$symfony.redis_prefix|g" /etc/supervisor/conf.d/supervisord.conf > /etc/supervisor/conf.d/supervisord.conf
+        sed -e "s|REDISHOST|$symfony.redis_host|g" /etc/supervisor/conf.d/supervisord.conf > /etc/supervisor/conf.d/supervisord.conf
+        sed -e "s|REDISPORT|$symfony.redis_port|g" /etc/supervisor/conf.d/supervisord.conf > /etc/supervisor/conf.d/supervisord.conf
+        sed -e "s|REDISDB|$symfony.redis_database|g" /etc/supervisor/conf.d/supervisord.conf > /etc/supervisor/conf.d/supervisord.conf
     fi
 fi
 
